42903e610d367271717ab90b740771d22e6e6109
xizaixuan <285963400@qq.com>
2017-08-29 21:38:48 +0800

    add scene and Ocean scripts

---
+++ Ocean for Unity/Assets/Scripts/Ocean.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Ocean : MonoBehaviour
+{
+
+	public bool test = false;
+
+	void Awake()
+	{
+        test = true;
+    }
+
+    void Start()
+    {
+        test = false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
